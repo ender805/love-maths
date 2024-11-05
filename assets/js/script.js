9e198c7841dfee6b1004faa81282d0c106e439f8
Maciej Tuszynski <xtzl9j78@students.codeinstitute.net>
2024-11-05 23:59:52 +0000

    function

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function runGame () {
+
+}
+
+function checkGame () {
+
+}
+
+function calculateCorrectAnswer () {
+
+}
+function IncrementScore () {
+
+}
+
+function IncrementWrongAnswer () {
+
+}
+
+function displayAdditionalQuestion () {
+
+}
+function displaySubstractQuestion () {
+
+}
+
+function displayMultiplyQuestion () {
+
+}
