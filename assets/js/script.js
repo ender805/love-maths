8de5f8d0c2af50aab482b61e50917937de68e7a5
Maciej Tuszynski <xtzl9j78@students.codeinstitute.net>
2024-11-06 12:02:14 +0000

    correct answers function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -37,16 +37,37 @@ function runGame(gameType) {
     }
 
 }
-
+// check the answer agains the first eleme in teh returned calculateCorrectAnswer array
 function checkAnswer() {
+
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
 
 }
 // gets the operands (the numbers) and the operatore (plus, minus etc)
 // directly from the DOMException, and returns the correct ansewer
 function calculateCorrectAnswer() {
-    let operand1 = parseInt(document.getElementById(`operand1`).innerText);
-    let operand2 = parseInt(document.getElementById(`operand2`).innerText);
-    let operator = document.getElementsById("operator").innerText;
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
+
 }
 
 function incrementScore() {
